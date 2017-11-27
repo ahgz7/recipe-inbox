d2e993e752c2ccfc940c990c4b21c2fd1eaf2e6b
AhmeD HegazY <ahmedhegazy835@gmail.com>
2017-11-27 20:25:03 +0100

    Added unread Hangout messages to the count

--- webview.js
+++ webview.js
@@ -1,10 +1,14 @@
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
     let count = 0;
-
+    
+    // Unread emails
     if (document.querySelectorAll('.DsPmj').length > 0) {
       count = document.querySelectorAll('.DsPmj')[0].querySelectorAll('div.G3').length;
     }
+    
+    // Unread Hangout messages
+    count += document.querySelectorAll(".Ik.uB").length
 
     // set Franz badge
     Franz.setBadge(count);
